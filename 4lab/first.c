2a5d454105a0a9563805a8d98ec8b9e6dac7158e
Sergey <36138085+RedBird06@users.noreply.github.com>
2019-12-20 17:06:15 +0300

    add code

---
+++ 4lab/first.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <time.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <string.h>
+
+#define FILE "shmemochka"
+
+int main(){
+
+	key_t key = ftok(FILE, 1);
+	int smid = shmget(key, 32, IPC_CREAT|0666);
+	if (smid == -1)
+	{
+		perror("SHMEM CREATE FAILED\n");
+		return 0;
+	}
+
+	char* addr = shmat(smid, NULL, 0);
+	if (addr == (char*)-1)
+	{
+		perror("SHMAT FAILED\n");
+		return 0;
+	}
+
+	while (1)
+	{
+		time_t tt = time(0);
+		sprintf(addr, "pid: %d time: %s",getpid() ,ctime(&tt));
+		sleep(1);
+	}
+
+	return 0;
+}
---
+++ 4lab/second.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <time.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+
+#define FILE "shmemochka"
+
+int main(){
+        
+        key_t key = ftok(FILE, 1);
+        int smid = shmget(key, 32, 0666);
+        if (smid == -1)
+        {
+                perror("SHMEM CREATE FAILED\n");
+                return 0;
+        }
+
+        char* addr = shmat(smid, NULL, 0);
+        if (addr == (char*)-1){
+                perror("SHMAT FAILED\n");
+                return 0;
+        }
+
+        while (1)
+		{
+                sleep (1);
+				printf("%s", addr);
+        }
+
+        return 0;
+}
