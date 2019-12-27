b745f5035348285c86babb5b41c4e9aaf79534ec
Sergey <36138085+RedBird06@users.noreply.github.com>
2019-12-27 11:52:36 +0300

    fix

--- 6lab/main.c
+++ 6lab/main.c
@@ -16,53 +16,55 @@ void* thread_func()
 {
 	key_t key = ftok(FILE, 1);
 	int shmid = shmget(key, buffer_size, 0666);
-	if (smid == -1)
+	if (shmid == -1)
 	{
 		perror("SHMEM CREATE FAILED\n");
 		return 0;
 	}
 	char* addr = shmat(shmid, NULL, 0);
-	if (addr == (char*)-1){
+	if (addr == (char*)-1)
+	{
 		printf ("SHMAT FAILED\n");
 		return 0;
 	}
-	sleep(1);
 	while (1)
 	{
-        	pthread_mutex_lock(&mutex);
+    pthread_mutex_lock(&mutex);
 		printf("TID: %ld Say:%s",pthread_self(),addr);
-        	pthread_mutex_unlock(&mutex);
-		sleep (1);
+    pthread_mutex_unlock(&mutex);
+		sleep(1);
 	}
 	return 0;
 }
 
 int main()
 {
-	
 	pthread_t pth[10];
 	int i;
-	int counter;
+	int counter = -1;
 	pthread_mutex_init(&mutex, NULL);
 	key_t key = ftok(FILE, 1);
-	int shmid = shmget(key, 32, IPC_CREAT|0666);
+	int shmid = shmget(key, buffer_size, IPC_CREAT|0666);
 	if (shmid == -1)
 	{
 		perror("SHMEM CREATE FAILED\n");
 		return 0;
-	}
-	
+	}	
 	char* addr = shmat(shmid, NULL, 0);
-	if (addr == (char*)-1){
+	if (addr == (char*)-1)
+	{
 		printf ("SHMAT FAILED\n");
 		return 0;
 	}
-	
+
+	sprintf(addr, "%d\n", counter);
+
 	for(i=0;i<10;++i)
 	{
 		int stat = pthread_create(&pth[i], NULL, thread_func, NULL);
-		if (stat != 0){
-			printf ("%d thread create fialed\n", stat);
+		if (stat != 0)
+		{
+			printf ("%d THREAD CREATE FAILED\n", stat);
 			return 0;
 		}
 	}
